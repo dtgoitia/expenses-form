84b32283caac0020b6e3b08024c0bcefe1f3fc05
David Torralba Goitia <david.torralba.goitia@gmail.com>
2022-05-20 08:43:26 +0100

    Hide time picker by default

--- webapp/src/components/TimePicker.tsx
+++ webapp/src/components/TimePicker.tsx
@@ -1,7 +1,16 @@
+import { Button, Collapse } from "@blueprintjs/core";
 import {
   TimePicker as BlueprintTimePicker,
   TimePrecision,
 } from "@blueprintjs/datetime";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 1rem;
+  display: flex;
+  justify-content: space-between;
+`;
 
 interface Props {
   date: Date;
@@ -10,14 +19,25 @@ interface Props {
 }
 
 function TimePicker({ defaultDate, date, onChange }: Props) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <BlueprintTimePicker
-      value={date}
-      defaultValue={defaultDate}
-      showArrowButtons
-      precision={TimePrecision.SECOND}
-      onChange={onChange}
-    />
+    <Container>
+      <Collapse isOpen={isOpen}>
+        <BlueprintTimePicker
+          value={date}
+          defaultValue={defaultDate}
+          showArrowButtons
+          precision={TimePrecision.SECOND}
+          onChange={onChange}
+        />
+      </Collapse>
+
+      <Button
+        text={isOpen ? "Close" : "Edit time"}
+        onClick={() => setIsOpen(!isOpen)}
+      />
+    </Container>
   );
 }
 
