c4b41517ba41166a830c7fd73a6477a8eaaf0e7c
David Torralba Goitia <david.torralba.goitia@gmail.com>
2024-10-12 17:00:39 +0100

    fix: remove unused pre/post build scripts

--- webapp/src/constants.ts
+++ webapp/src/constants.ts
@@ -58,28 +58,4 @@ export const TAGS = [
 ];
 
 export const DEFAULT_PEOPLE = ["AngelaPerez"];
-export const SELLERS = [
-  "Amazon",
-  "AWS",
-  "GiffGaff",
-  "Lidl",
-  "Morrisons",
-  "Sainsbury",
-  "Tesco",
-  "TfL",
-];
-
 export const BASE_URL = "expenses-form"; // must match "homepage" field in package.json
-export const SPLITWISE_API_BASE_URL = "foo";
-// export const SPLITWISE_API_BASE_URL = LoadEnvVar.asUrl(
-//   "REACT_APP_SPLITWISE_API_BASE_URL"
-// );
-export const MOCK_APIS = false;
-// export const MOCK_APIS = LoadEnvVar.asBoolean("REACT_APP_MOCK_APIS");
-export const DEVELOPMENT_MODE = process.env.NODE_ENV === "development";
-
-export enum ApiEndpoints {
-  OAUTH_TOKEN = "/auth/token/",
-  OAUTH_VALIDATE_TOKEN = "/api/v1/test/",
-  USERS_TO_SUSPEND = "/api/v1/users-to-suspend/",
-}
