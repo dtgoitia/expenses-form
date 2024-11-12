e3f580e4fee3a9eb24bb2e91e61c43df65fb2e54
David Torralba Goitia <david.torralba.goitia@gmail.com>
2024-11-12 07:38:28 +0000

    feat(collapse): create `Collapse` component

---
+++ webapp/src/components/Collapse.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react";
+
+interface Props {
+  /**
+   * Content to collapse.
+   */
+  children: ReactNode | ReactNode[];
+
+  /**
+   * If `true`, the content is visible (open), otherwise the content is
+   * hidden (collapsed).
+   */
+  isOpen: boolean;
+}
+export function Collapse({ children, isOpen }: Props) {
+  const css = `
+    overflow-hidden
+    transition-all
+    duration-300
+    ease-in-out
+    ${isOpen ? "max-h-96" : "max-h-0"}
+  `;
+  return (
+    <div className={css} role="collapse">
+      {children}
+    </div>
+  );
+}
