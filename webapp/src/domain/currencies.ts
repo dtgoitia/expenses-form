a2c838425cba5a68fc737ba8e1d96b5d9f789bb2
David Torralba Goitia <david.torralba.goitia@gmail.com>
2025-12-19 12:07:22 +0700

    feat(currencies): handle currency addition and unsafe-deletion at `CurrencyManager`

--- webapp/src/domain/currencies.ts
+++ webapp/src/domain/currencies.ts
@@ -1,5 +1,5 @@
-import { CurrencyCode } from "./model";
 import { Observable, Subject } from "rxjs";
+import { CurrencyCode } from "./model";
 
 export class CurrencyManager {
   public change$: Observable<CurrencyChanges>;
@@ -27,8 +27,38 @@ export class CurrencyManager {
   public getAll(): Set<CurrencyCode> {
     return new Set([...this.currencies.values()]);
   }
+
+  public add(currency: CurrencyCode): AddCurrencyResult {
+    if (this.currencies.has(currency)) {
+      return { ok: false, reason: `currency ${currency} already exists` };
+    }
+
+    this.currencies.add(currency);
+
+    this.changeSubject.next({ kind: "CurrencyAdded", currency });
+    return { ok: true };
+  }
+
+  /* Internal API: use App.deleteCurrencySafe instead */
+  public deleteUnsafe(currency: CurrencyCode): DeleteCurrencyResult {
+    const success = this.currencies.delete(currency);
+    if (success === false) {
+      return {
+        ok: false,
+        reason: `currency ${currency} was not deleted because it did not exist`,
+      };
+    }
+
+    this.changeSubject.next({ kind: "CurrencyDeleted", currency });
+    return { ok: true };
+  }
 }
+
+export type AddCurrencyResult = { ok: true } | { ok: false; reason: string };
+
+export type DeleteCurrencyResult = { ok: true } | { ok: false; reason: string };
 
 export type CurrencyChanges =
   | { kind: "CurrencyManagerInitialized" }
-  | { kind: "CurrencyAdded"; currency: CurrencyCode };
+  | { kind: "CurrencyAdded"; currency: CurrencyCode }
+  | { kind: "CurrencyDeleted"; currency: CurrencyCode };
